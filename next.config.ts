4d145749985ec9ed7d4924f7c1996bab8e5bd570
Todd MacDonald <Toddwmac@gmail.com>
2026-01-28 15:31:08 -0700

    AssetPrefix chante

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
+  basePath: '/kilo-demo-2',
+  assetPrefix: '/kilo-demo-2',
   images: {
     unoptimized: true,
   },
